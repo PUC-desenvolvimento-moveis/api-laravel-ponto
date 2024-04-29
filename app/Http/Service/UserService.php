1036e67779f90dfe24b14429d2681385a6ef6d19
ign7 <quaresmaigor@yahoo.com>
2024-04-29 20:54:39 -0300

    (feat)[service,controller] crud methods

--- app/Http/Service/UserService.php
+++ app/Http/Service/UserService.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 use App\Models\User;
 
-class UserService 
+class UserService
 {
     public function  index()
     {
@@ -24,10 +24,10 @@ class UserService
     {
         $user = new User;
         $user->name = $request->name;
-        $user->password=$request->password;
-        $user->email=$request->email;
-        $user->cpf=$request->cpf;
-        $user->telefone=$request->telefone;
+        $user->password = $request->password;
+        $user->email = $request->email;
+        $user->cpf = $request->cpf;
+        $user->telefone = $request->telefone;
         //$user->course = $request->course;
         $user->save();
 
@@ -38,10 +38,30 @@ class UserService
 
     public function update(Request $request, $id)
     {
-        
+        $user = User::find($id);
+        if (!$user) {
+            return response()->json(["message" => "User not found"], 404);
+        }
+        $user->name = $request->name ?? $user->name;
+        $user->password = $request->password ?? $user->password;
+        $user->email = $request->email ?? $user->email;
+        $user->cpf = $request->cpf ?? $user->cpf;
+        $user->telefone = $request->telefone ?? $user->telefone;
+        $user->save();
+        return response()->json([
+            "message" => "User updated successfully"
+        ], 200);
     }
 
     public function destroy($id)
     {
+        $user = User::find($id);
+        if (!$user) {
+            return response()->json(["message" => "User not found"], 404);
+        }
+        $user->delete();
+        return response()->json([
+            "message" => "User deleted successfully"
+        ], 200);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('users', 'UserController@index');
 Route::post('users', 'UserController@store');
 Route::get('users/{id}', 'UserController@show');
